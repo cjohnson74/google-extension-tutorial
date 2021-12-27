53f59ebf67c42f06ef05ca02817c79a7bdc874fe
Carson Johnson <carson74johnson@gmail.com>
2021-12-27 14:05:02 -0500

    added make the currently selected color

--- options.js
+++ options.js
@@ -31,6 +31,11 @@ function constructOptions(buttonColors) {
             button.dataset.color = buttonColor;
             button.style.backgroundColor = buttonColor;
 
+            // ...mark the currently selected color...
+            if (buttonColor === currentColor) {
+                button.classList.add(selectedClassName);
+            }
+
             // ...and register a listener for when that button is clicked
             button.addEventListener("click", handleButtonClick);
             page.appendChild(button);
